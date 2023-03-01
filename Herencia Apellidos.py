40f58472c27a5eecbb39679ffb9937cc3d445437
JonathanRCM <118033489+JonathanRCM@users.noreply.github.com>
2023-02-28 22:00:36 -0600

    Add files via upload

---
+++ Herencia Apellidos.py
@@ -0,0 +1,22 @@
+class Persona():
+
+    def __init__(self, apePat, apeMat, nom):
+        self.apellidoPaterno=apePat
+        self.apellidoMaterno=apeMat
+        self.nombre=nom
+
+    def mostrarNombreCompleto(self):
+        txt="{0} {1} {2}"
+        return txt.format(self.apellidoPaterno, self.apellidoMaterno, self.nombre)
+    
+class Estudiante(Persona):
+
+    def __init__(self, apePat, apeMat, nom, pro):
+        super().__init__(apePat, apeMat, nom) 
+        self.profesion=pro
+
+estudi1=Estudiante("Torres", "Lopez", "Juan", "Ingenieria en Sistemas")
+print(estudi1.mostrarNombreCompleto())           
+print(estudi1.profesion)
+    
+        
